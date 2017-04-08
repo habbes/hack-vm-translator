aa7a2b648f44c5dfcd994183dfa77222ebfb9d9d
Clement Habinshuti <hello@habbes.xyz>
2017-04-08 17:48:06 +0300

    Add tests and fix bugs in tests

--- test/vm_translator/parser_test.clj
+++ test/vm_translator/parser_test.clj
@@ -69,11 +69,71 @@
       (let [source "cmd 1" cmd (match-and-parse source re-f-pairs)]
         (is (= {:source "cmd 1"
                 :command "cmd"
-                :arg "1"})))
+                :arg "1"}
+               cmd)))
       (let [source "cmd" cmd (match-and-parse source re-f-pairs)]
         (is (= {:source "cmd"
-                :command "cmd"}))))
+                :command "cmd"}
+               cmd))))
     (testing "returns in nil if there is no match"
       (let [source "this has no match" cmd (match-and-parse source re-f-pairs)]
         (is (= nil cmd))))))
+
+(deftest parse-command-with-def-matchers
+  (let [segments ["arg" "this" "that" "local"
+                  "constant" "temp" "static" "pointer"]]
+    (testing "push commands"
+      (doseq [segment segments]
+        (let [source (str "push " segment " 2")
+              cmd (parse-command source)]
+          (is (= {:source source
+                  :command "push"
+                  :segment segment
+                  :index "2"}
+                 cmd)))))
+    (testing "pop commands"
+      (doseq [segment segments]
+        (let [source (str "pop " segment " 2")
+              cmd (parse-command source)]
+          (is (= {:source source
+                  :command "pop"
+                  :segment segment
+                  :index "2"}
+                 cmd))))))
+  (testing "arithmetic/logic commands"
+    (doseq [source ["add" "sub" "neg" "eq" "gt" "lt" "and" "or" "not"]]
+      (let [cmd (parse-command source)]
+        (is (= {:source source
+                :command source}
+               cmd)))))
+  (testing "ignores comments and extra whitespace"
+    (let [source "push local 10 //test" cmd (parse-command source)]
+      (is (= {:source "push local 10"
+              :command "push"
+              :segment "local"
+              :index "10"}
+             cmd)))
+    (let [source "\tadd  " cmd (parse-command source)]
+      (is (= {:source "add"
+              :command "add"}
+             cmd)))
+    (let [source " pop  pointer  0 // comment" cmd (parse-command source)]
+      (is (= {:source "pop pointer 0"
+              :command "pop"
+              :segment "pointer"
+              :index "0"
+              cmd}))))
+  (testing "returns nil if no command or no match"
+    (let [source "" cmd (parse-command source)]
+      (is (= nil cmd)))
+    (let [source "   " cmd (parse-command source)]
+      (is (= nil cmd)))
+    (let [source "// this is a comment" cmd (parse-command source)]
+      (is (= nil cmd)))
+    (let [source "\t // this is a comment" cmd (parse-command source)]
+      (is (= nil cmd)))
+    (let [source "push local" cmd (parse-command source)]
+      (is (= nil cmd)))
+    (let [source "this is not a command// push arg 1" cmd (parse-command source)]
+      (is (= nil cmd)))))
 
