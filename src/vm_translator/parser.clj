1fcd0f90f7d2d4f83daa17a52013ce44ea924be2
Clement Habinshuti <hello@habbes.xyz>
2017-04-08 18:18:29 +0300

    Add parser-command function and fix bugs

--- src/vm_translator/parser.clj
+++ src/vm_translator/parser.clj
@@ -7,14 +7,6 @@
 ; regex used to parse push and pop commands
 (def push-pop-re #"(push|pop) (arg|local|this|that|constant|static|temp|pointer) (\d+)")
 
-(declare parse-arithmetic-command)
-(declare parse-push-pop-command)
-
-;Pairs of regexes and their corresponding parser fns
-(def re-parser-pairs
-  [[arithmetic-re parse-arithmetic-command]
-   [push-pop-re parse-push-pop-command]])
-
 (defn clean-line
   "Cleans source line, removing comments and extra whitespace"
   [line]
@@ -34,6 +26,11 @@
   [cmd-ctx [segment index]]
   (assoc cmd-ctx :segment segment :index index))
 
+;Pairs of regexes and their corresponding parser fns
+(def re-parser-pairs
+  [[arithmetic-re parse-arithmetic-command]
+   [push-pop-re parse-push-pop-command]])
+
 (defn parse-if-match
   "Parses the source with the given function f if source matches
   the given regex re. f takes a command ctx map and
@@ -47,16 +44,16 @@
   "Tests source lazily against each regex in re-parser-pairs and return
   the result cmd-ctx of the first match or nil"
   [source re-f-pairs]
-  (first
-    (map
-      (fn [[re f]] (parse-if-match source re f))
-      re-f-pairs)))
+  (->> re-f-pairs
+       (map (fn [[re f]] (parse-if-match source re f)))
+       (filter identity)
+       (first)))
 
 (defn parse-command
   "Parses a line of vm source code into a command context object based
   on the provided regex and parser fns"
   ([source]
-   (parse-command re-parser-pairs))
+   (parse-command source re-parser-pairs))
   ([source re-f-pairs]
   (-> source
       clean-line
