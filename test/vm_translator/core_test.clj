018077339fb2464c714f71156dca2d329348ec9c
Clement Habinshuti <hello@habbes.xyz>
2017-04-12 10:01:12 +0300

    Add test cases for translate-lines fn

--- test/vm_translator/core_test.clj
+++ test/vm_translator/core_test.clj
@@ -3,6 +3,41 @@
             [vm-translator.core :refer :all]
             [clojure.string :as s]))
 
+(def sample-source
+"push constant 5
+push constant 3
+add")
+
+(def sample-output
+"// push constant 5
+@5
+D=A
+@SP
+A=M
+M=D
+D=A+1
+@SP
+M=D
+// push constant 3
+@3
+D=A
+@SP
+A=M
+M=D
+D=A+1
+@SP
+M=D
+// add
+@SP
+A=M-1
+D=M
+A=A-1
+M=M+D
+D=A
+@SP
+M=D
+")
+
 (deftest get-output-path-test
   (testing "Replace filename extension with .asm"
     (let [input "Sample.vm" out (get-output-path input)]
@@ -38,3 +73,18 @@
   (testing "Returns nil on when source is invalid"
     (let [line "not valid command" out (translate-line line)]
       (is (= out nil)))))
+
+(defn create-sample-output-handler
+  "Creates an output handler fn that appends the output
+  to the specified out-atom"
+  [out-atom]
+  (fn [out]
+    (swap! out-atom (fn [old] (str old out)))))
+
+(deftest translate-lines-test
+  (testing ""
+    (let [lines (s/split sample-source #"\n")
+          output (atom "")
+          handler (create-sample-output-handler output)]
+      (translate-lines lines handler)
+      (is (= @output sample-output)))))
