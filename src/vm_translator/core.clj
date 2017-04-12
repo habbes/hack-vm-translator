ec47538985d3598cf171ad664729eb5351aea0b8
Clement Habinshuti <hello@habbes.xyz>
2017-04-12 08:44:07 +0300

    Write code to translate source file to output file

--- src/vm_translator/core.clj
+++ src/vm_translator/core.clj
@@ -1,8 +1,49 @@
 (ns vm-translator.core
-  (:require [vm-translator.parser :as parser])
+  (:require [vm-translator.parser :as parser]
+            [vm-translator.code :as code]
+            [clojure.string :as s]
+            [clojure.java.io :as io])
   (:gen-class))
+
+(defn translate-line
+  "Translates line of vm source code to hack assembly code.
+  Returns nil if source code is invalid."
+  [line]
+  (if-let [cmd (parser/parse-command line)]
+    (code/translate-with-comment line)
+    nil))
+
+(defn translate-source
+  "Reads vm source code from rdr and writes the output
+  assembly code into wrtr"
+  [rdr wrtr]
+  (let [lines (line-seq rdr)]
+    (loop [count 0]
+      (if-let [line (nth lines count nil)]
+        (if-let [out (translate-line line)]
+          (do (.write wrtr out)
+            (recur (inc count)))
+          nil)
+        nil)))
+
+(defn get-output-path
+  "Get the output path for the output file based on
+  the input vm file."
+  [input-path]
+  (s/replace #"([a-zA-Z0-9_- ]+)\.vm$" "$1.asm"))
+
+(defn translate-file
+  "Translate the input vm file and store the asm output in
+  the specified output file"
+  [input-path output-path]
+  (with-open [rdr (io/reader input-path)]
+    (with-open [wrtr (io/writer output-path)]
+      (prn "Translating to" output-path)
+      (translate-source rdr wrtr)
+      (prn "Operation complete"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [input-path]
+  (let [output-path (get-output-path input-path)]
+    (translate-file input-path output-path)))
