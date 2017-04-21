917ebcc21c3307f623febe85afe4687fa625e0de
Clement Habinshuti <hello@habbes.xyz>
2017-04-21 23:04:10 +0300

    Add functions to manipulate class and function names in context

--- src/vm_translator/context.clj
+++ src/vm_translator/context.clj
@@ -1,17 +1,41 @@
 (ns vm-translator.context)
 
 (defn initialize
+  "Initializes new context"
   [class-name]
   {:line-number 0
    :instruction-number -1
    :class class-name})
 
 (defn inc-line
+  "Increments the line number"
   [ctx]
   (update ctx :line-number inc))
 
 (defn inc-instruction
+  "Increments the instruction number by the specifed count"
   ([ctx]
    (inc-instruction ctx 1))
   ([ctx count]
-   (update ctx :instruction-number (partial + count))))
+   (update ctx
+           :instruction-number (partial + count))))
+
+(defn set-class
+  "Sets the current class for the context"
+  [ctx class-name]
+  (assoc ctx :class class-name))
+
+(defn unset-class
+  "Removes the current class from the context"
+  [ctx]
+  (dissoc ctx :class))
+
+(defn set-function
+  "Sets the current function for the context"
+  [ctx function]
+  (assoc ctx :function function))
+
+(defn unset-function
+  "Removes the current function from the context"
+  [ctx]
+  (dissoc ctx :function))
--- test/vm_translator/context_test.clj
+++ test/vm_translator/context_test.clj
@@ -37,3 +37,69 @@
       (is (= new-ctx {:line-number 6
                       :instruction-number 16
                       :class "Class"})))))
+
+(deftest set-class-test
+  (testing "Adds class key to context"
+    (let [ctx {:line-number 1
+               :instruction-number 0}
+          new-ctx (set-class ctx "SomeClass")]
+      (is (= new-ctx {:line-number 1
+                      :instruction-number 0
+                      :class "SomeClass"}))))
+  (testing "Replaces existing class"
+    (let [ctx {:line-number 1
+               :instruction-number 0
+               :class "Sample"}
+          new-ctx (set-class ctx "Other")]
+      (is (= new-ctx {:line-number 1
+                      :instruction-number 0
+                      :class "Other"})))))
+
+(deftest unset-class-test
+  (testing "Removes class key from context"
+    (let [ctx {:line-number 1
+               :instruction-number 0
+               :class "Math"}
+          new-ctx (unset-class ctx)]
+      (is (= new-ctx {:line-number 1
+                      :instruction-number 0}))))
+  (testing "Does nothing if class key was not set"
+    (let [ctx {:line-number 1
+               :instruction-number 0}
+          new-ctx (unset-class ctx)]
+      (is (= new-ctx ctx)))))
+
+(deftest set-function-test
+  (testing "Adds function key to the context"
+    (let [ctx {:line-number 1
+               :instruction-number 0
+               :class "MyClass"}
+          new-ctx (set-function ctx "MyClass.func")]
+      (is (= new-ctx {:line-number 1
+                      :instruction-number 0
+                      :class "MyClass"
+                      :function "MyClass.func"}))))
+  (testing "Replaces existing function"
+    (let [ctx {:line-number 1
+               :instruction-number 0
+               :class "MyClass"
+               :function "MyClass.func"}
+          new-ctx (set-function ctx "MyClass.newFunc")]
+      (is (= new-ctx {:line-number 1
+                      :instruction-number 0
+                      :class "MyClass"
+                      :function "MyClass.newFunc"})))))
+
+(deftest unset-function-test
+  (testing "Removes function key from context"
+    (let [ctx {:line-number 1
+               :instruction-number 0
+               :function "mult"}
+          new-ctx (unset-function ctx)]
+      (is (= new-ctx {:line-number 1
+                      :instruction-number 0}))))
+  (testing "Does nothing if function key was not set"
+    (let [ctx {:line-number 1
+               :instruction-number 0}
+          new-ctx (unset-function ctx)]
+      (is (= new-ctx ctx)))))
